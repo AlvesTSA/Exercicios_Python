be41d75ccf8342a48025b517518e7f9abe52595e
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-04-02 09:51:42 -0300

    exercicio_4

---
+++ Estrutura_Sequencial/exercicio_4.py
@@ -0,0 +1,21 @@
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+print('Digite a nota do primeiro bimestre')
+
+nota1 = float(input())
+
+print('Digite a nota do segundo bimestre')
+
+nota2 = float(input())
+
+print('Digite a nota do terceiro bimestre')
+
+nota3 = float(input())
+
+print('Digite a nota do quarto bimestre')
+
+nota4 = float(input())
+
+media = (nota1+nota2+nota3+nota4)/4
+
+print('Sua nota final é: {}'.format(media))
