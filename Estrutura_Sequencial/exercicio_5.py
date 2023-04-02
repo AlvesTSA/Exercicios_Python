6997f7e128aabd29d8c08aa77a1315ab0e78db93
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-04-02 10:08:54 -0300

    exercicio_5

---
+++ Estrutura_Sequencial/exercicio_5.py
@@ -0,0 +1,9 @@
+#Faça um Programa que converta metros para centímetros.
+
+print('Digite uma medida em metros')
+
+metros = float(input())
+
+centimetros = metros*100
+
+print('a medida em centímetro é: {} cm' .format(centimetros))
