08ad7d460adaccd18c570d2690788ee80794e5a8
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-04-02 09:35:09 -0300

    exercicio_3

---
+++ Estrutura_Sequencial/exercicio_3.py
@@ -0,0 +1,13 @@
+#Faça um Programa que peça dois números e imprima a soma.
+
+print("Digite o primeiro número")
+
+numero1 = float(input())
+
+print("Digite o segundo número")
+
+numero2 = float(input())
+
+soma = numero1 + numero2
+
+print("O resultado da soma é: {}" .format(soma))
