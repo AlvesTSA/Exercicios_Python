517f73f8555c685bb30db01efb92b2dece7a9339
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-04-03 09:43:07 -0300

    exercicio_7

---
+++ Estrutura_Sequencial/exercicio_7.py
@@ -0,0 +1,10 @@
+#Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+print('Digite o comprimento de um dos lados do quadrado')
+
+comprimento = float(input())
+
+area = (comprimento*comprimento)*2
+
+print('O dobro da área do quadrado é : {}' .format(area))
+
