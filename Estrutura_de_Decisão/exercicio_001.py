246449a639db6d5a7071f32183a507ac5ffa7dcc
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-05-06 17:09:24 -0300

    exercicio_1

---
+++ Estrutura_de_Decisão/exercicio_001.py
@@ -0,0 +1,22 @@
+#  Faça um Programa que peça dois números e imprima o maior deles.
+
+num1 = 0
+num2 = 0
+
+print("Informe dois números: ")
+num1 = int(input())
+num2 = int(input())
+maior = 0
+
+if num1 > num2:
+
+    maior = num1
+
+else :
+
+    maior = num2
+
+print("Maior: " + str(maior))
+
+
+
