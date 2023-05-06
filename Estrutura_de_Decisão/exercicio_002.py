a18a8fe8c106c18df5e43e016f15e48fefdeafa7
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-05-06 17:13:32 -0300

    exercicio_2

---
+++ Estrutura_de_Decisão/exercicio_002.py
@@ -0,0 +1,16 @@
+# Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+valor = 0
+
+print("Informe um valor: ")
+valor = int(input())
+
+if valor > 0 :
+
+    print("POSITIVO")
+
+else :
+
+    print("NEGATIVO")
+
+    
