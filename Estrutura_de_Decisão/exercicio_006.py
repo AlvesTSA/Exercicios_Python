0cefa1168acb0d891ee2c77b44ad23fdc14c7320
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-05-06 23:12:37 -0300

    exercicio_6

---
+++ Estrutura_de_Decisão/exercicio_006.py
@@ -0,0 +1,30 @@
+#  Faça um Programa que leia três números e mostre o maior deles.
+
+num1 = 0
+num2 = 0
+num3 = 0
+maior = 0
+
+print("Informe três números: ")
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+if num1 > num2 and num1 > num3 :
+
+    maior = num1
+
+elif num2 > num1 and num2 > num3 :
+
+    maior = num2
+
+else  :
+
+    maior = num3
+
+print("Maior: " + str(maior))
+
+
+
+
+
