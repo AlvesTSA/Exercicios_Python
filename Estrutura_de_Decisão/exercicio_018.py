e668af2cf3976ec41bbfb408be44e67789a7834c
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-05-28 16:28:28 -0300

    exercicio_18

---
+++ Estrutura_de_Decisão/exercicio_018.py
@@ -0,0 +1,14 @@
+#Faça um Programa que peça um número inteiro e determine se ele e par ou ímpar.
+
+num = 0
+
+print("Informe um número inteiro: ")
+num = int(input())
+
+if (num % 2 == 0):
+
+    print("Número par")
+
+else:
+
+    print("Número ímpar.")
