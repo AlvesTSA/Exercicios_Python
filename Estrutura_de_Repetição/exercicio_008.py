0138cb1d3fc41abf2698a78b275d44630d1775fb
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-08-30 08:16:19 -0300

    Create exercicio_008.py

---
+++ Estrutura_de_Repetição/exercicio_008.py
@@ -0,0 +1,18 @@
+#8. Faça um programa que leia 5 números e informe a soma e a média dos números.
+
+num = 0
+soma = 0
+media = 0
+i = 0
+
+print("Informe 5 números:")
+
+for num in range(1, 6):
+
+    num = float(input())
+    i += 1
+    soma += num
+
+media = soma/i
+
+print("Soma = {}\nMedia = {}".format(soma, media))
